765a531c92dcfa633f8e1b23c3f7e8d45b5f64b6
GeraltOfRivia3991 <106952994+San2022-117@users.noreply.github.com>
2022-09-03 12:07:29 +0200

    small set up edits

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Weather App for the SheCodes programming course.</p>
-        <a
-          href="https://github.com/San2022-117/sc-weather-project"
-          alt="gitHub repository"
-          target="_blank"
-        >
-          GitHub repository for this poject.
-        </a>
+        <h1>Weather App</h1>
+        <h3>for the SheCodes programming course</h3>
+
+        <footer>
+          My GitHub{" "}
+          <a
+            href="https://github.com/San2022-117/sc-weather-project"
+            alt="gitHub repository"
+            rel="noreferrer"
+            target="_blank"
+          >
+            repository
+          </a>{" "}
+          for this poject.
+        </footer>
       </header>
     </div>
   );
