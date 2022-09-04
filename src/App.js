d8c2f9a4a8d9dd50ec650be560571c92a33f0e3e
GeraltOfRivia3991 <106952994+San2022-117@users.noreply.github.com>
2022-09-04 09:09:16 +0200

    Added bootstrap
    npm i bootstrap

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <h3>for the SheCodes programming course</h3>
-
-        <footer>
-          My GitHub{" "}
-          <a
-            href="https://github.com/San2022-117/sc-weather-project"
-            alt="gitHub repository"
-            rel="noreferrer"
-            target="_blank"
-          >
-            repository
-          </a>{" "}
-          for this poject.
-        </footer>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather App</h1>
+          <h3>for the SheCodes programming course</h3>
+          <footer>
+            My GitHub{" "}
+            <a
+              href="https://github.com/San2022-117/sc-weather-project"
+              alt="gitHub repository"
+              rel="noreferrer"
+              target="_blank"
+            >
+              repository
+            </a>{" "}
+            for this poject.
+          </footer>
+        </header>
+      </div>
     </div>
   );
 }
